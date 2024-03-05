2d241d4541a818a6eb762c34243484a5ba9d9f05
Skylar Asoka <57451015+hanasoka@users.noreply.github.com>
2024-03-05 14:41:25 +0000

    add html files

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+TICKETS = [
+  {
+    "id": 1,
+    "title": "From Non-Tech to Tech",
+    "category": "Personal Development",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  },
+  {
+    "id": 2,
+    "title": "Why I Learn to Code?",
+    "category": "Programming",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  },
+  {
+    "id": 3,
+    "title": "What Causes Monsoon?",
+    "category": "Curiosity Chronicles",
+  },
+  {
+    "id": 4,
+    "title": "Lang Tengah Island",
+    "category": "Projects",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  } 
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('home.html', tickets=TICKETS, name="Merang Jetty Boat Ticket Online")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
